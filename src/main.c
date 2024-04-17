f0e33c548f639fc30ee80fb70b54c8d5fbf821e4
Grzegorz Czernatowicz <gczernatowicz1@proton.me>
2024-04-17 21:34:12 +0200

    Initial config and hello blink test

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "sdkconfig.h"
+
+static const char *TAG = "OTDS Main";
+
+#define BLINK_GPIO 10
+
+static uint8_t s_led_state = 0;
+
+static void blink_led(void)
+{
+    gpio_set_level(BLINK_GPIO, s_led_state);
+}
+
+static void configure_led(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+}
+
+void app_main(void)
+{
+    configure_led();
+
+    while (1) {
+        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+        blink_led();
+        s_led_state = !s_led_state;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
